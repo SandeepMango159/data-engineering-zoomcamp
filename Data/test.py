7abdf9831d614a4005b28ae8c6bad8fd59f2d6f5
Sandeep Singh <sandeep159@outlook.com>
2025-05-15 17:41:21 +0200

    Script to print column data from csv

---
+++ Data/test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("C:/Sandeep SSD/Programming SSD/Data Engineering Zoomcamp/data-engineering-zoomcamp/Data/data/csv/fhv_tripdata_2019-06.csv/fhv_tripdata_2019-06.csv")
+print(df.dtypes)
+
+
