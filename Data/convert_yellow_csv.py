294fffcecac397c03396972bba3b7c409875ebde
Sandeep Singh <sandeep159@outlook.com>
2025-05-17 19:40:53 +0200

    Fix convert yellow csv to parquet

--- Data/convert_yellow_csv.py
+++ Data/convert_yellow_csv.py
@@ -13,17 +13,20 @@ def convert_yellow_csv_to_parquet(csv_gz_path):
     df = pd.read_csv(csv_gz_path, compression="gzip")
 
     # Apply correct schema conversions
-    df['VendorID'] = df['VendorID'].astype('string')
+    df['VendorID'] = pd.to_numeric(df["VendorID"], errors="coerce").astype("Int64")
+
     df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'], errors='coerce').astype(str)
     df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'], errors='coerce').astype(str)
 
-    df['passenger_count'] = pd.to_numeric(df['passenger_count'], errors='coerce').astype('Int64')
-    df['trip_distance'] = pd.to_numeric(df['trip_distance'], errors='coerce')
-    df['RatecodeID'] = df['RatecodeID'].astype('string')
     df['store_and_fwd_flag'] = df['store_and_fwd_flag'].astype('string')
+
+    df['RatecodeID'] = pd.to_numeric(df["RatecodeID"], errors="coerce").astype("Int64")
     df['PULocationID'] = pd.to_numeric(df['PULocationID'], errors='coerce').astype('Int64')
     df['DOLocationID'] = pd.to_numeric(df['DOLocationID'], errors='coerce').astype('Int64')
-    df['payment_type'] = pd.to_numeric(df['payment_type'], errors='coerce').astype('Int64')
+
+    df['passenger_count'] = pd.to_numeric(df['passenger_count'], errors='coerce').astype('Int64')
+    df['trip_distance'] = pd.to_numeric(df['trip_distance'], errors='coerce')
+    
     df['fare_amount'] = pd.to_numeric(df['fare_amount'], errors='coerce')
     df['extra'] = pd.to_numeric(df['extra'], errors='coerce')
     df['mta_tax'] = pd.to_numeric(df['mta_tax'], errors='coerce')
@@ -31,6 +34,8 @@ def convert_yellow_csv_to_parquet(csv_gz_path):
     df['tolls_amount'] = pd.to_numeric(df['tolls_amount'], errors='coerce')
     df['improvement_surcharge'] = pd.to_numeric(df['improvement_surcharge'], errors='coerce')
     df['total_amount'] = pd.to_numeric(df['total_amount'], errors='coerce')
+
+    df['payment_type'] = pd.to_numeric(df['payment_type'], errors='coerce').astype('Int64')
     df['congestion_surcharge'] = pd.to_numeric(df['congestion_surcharge'], errors='coerce')
 
     # Save to Parquet
