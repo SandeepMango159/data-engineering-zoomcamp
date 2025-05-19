c63641d1964958230019abb4e46a3401530ce392
Sandeep Singh <sandeep159@outlook.com>
2025-05-20 01:18:07 +0200

    Added dim taxi trips for each taxi ride with calender info

---
+++ 04-analytics-engineering/my_project/my_taxi_rides_ny/models/core/dim_taxi_trips.sql
@@ -0,0 +1,48 @@
+{# Table to  create a dimension table for taxi trips, this one will hold #}
+{#  keep it physical; this table is reused a lot #}
+{# optional but helps in BigQuery #}
+{{ config(
+    materialized = 'table',          
+    cluster_by   = ['year', 'service_type']   
+) }}
+
+
+{# Get the green data, add service type Green to identify it form the yellow ones #}
+with green_tripdata as (
+    select *,
+    'Green' as service_type
+    from {{ ref('base_staging_green_tripdata_external') }}
+),
+yellow_tripdata as (
+    select *,
+    'Yellow' as service_type
+    from {{ ref('base_staging_yellow_tripdata_external') }}
+),
+{# Union to add the data together, just adds them as rows below, and works perfectly because we cleaned the data and made the same rows #}
+unioned_trips as (
+    select * from green_tripdata
+    union all
+    select * from yellow_tripdata
+)
+
+select
+    {# Primary key #}
+    unioned_trips.tripid,
+
+    {# Timestamps you’ll still need for granular checks #}
+    unioned_trips.pickup_datetime,
+    unioned_trips.dropoff_datetime,
+
+    unioned_trips.service_type,
+
+    {# Columns to helpl with dates and calenders #}
+    extract(year from unioned_trips.pickup_datetime) as year,
+    extract(quarter from unioned_trips.pickup_datetime) as quarter,
+    concat(
+        cast(extract(year from pickup_datetime) as string),
+        '/Q',
+        cast(extract(quarter from pickup_datetime) as string)) 
+        as year_quarter,
+    extract(month from pickup_datetime) as month,
+
+from unioned_trips
